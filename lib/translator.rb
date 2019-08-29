ca8012950116df0e5bfbf3be4e1b9bc92feaa9a1
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-29 14:23:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,6 +29,7 @@ def get_japanese_emoticon(path, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  emoticon_hash = load_library(path)
 end
