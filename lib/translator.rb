8eb72f3ba523fbcbe34bfd6db69d7426a2d381e9
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-29 14:11:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,23 +4,25 @@ require "pathname"
 path = Pathname.new("./lib/emoticons.yml")
 
 def load_library(path)
-emoticons = YAML.load_file(path)
+  emoticons = YAML.load_file(path)
   # code goes here
-emoticon_hash = {
+  emoticon_hash = {
      "get_meaning" => {},
      "get_emoticon" => {}
    }
   emoticons.each do |meaning, emoticons|
     english_emoticon = emoticons[0]
     japanese_emoticon = emoticons[-1]
-   emoticon_hash["get_meaning"][japanese_emoticon] = meaning
-   emoticon_hash["get_emoticon"][english_emoticon] = japanese_emoticon
- end
-emoticon_hash
+    emoticon_hash["get_meaning"][japanese_emoticon] = meaning
+    emoticon_hash["get_emoticon"][english_emoticon] = japanese_emoticon
+  end
+  emoticon_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
+  emoticons = YAML.load_file(path)
+
 end
 
 def get_english_meaning
