464177def8fc6a302f8edde6e959d0f2d2808ffa
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-29 13:28:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # require modules here
 require 'yaml'
-emoticons = YAML.load_file("emoticons.yml")
+emoticons = YAML.load_file("lib/emoticons.yml")
 
 def load_library(emoticons)
   # code goes here
